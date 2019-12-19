d18a402675b50c5f0b466e9007b2bcc985d66de3
matvich <matvienko.ivan.a@outlook.com>
2019-12-20 02:13:57 +0300

    bert_score fix2

--- bert_flair_score.py
+++ bert_flair_score.py
@@ -31,7 +31,7 @@ def main_bert():
     for refed, sampled in tqdm(zip(reference_answers, sampled_answers_gpt)):
         
         score_list_gpt.append(criterion(refed, sampled, embedder))
-	    clear_output(True)
+        clear_output(True)
         
     np.save('bert_scores_gpt.npy', np.array(score_list_gpt))
 
